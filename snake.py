21f8d7d052ee6dc46cb5fb17fd5a8b4bb325d975
namper <hoangducnam.work@gmail.com>
2023-10-13 21:50:41 +0700

    create snake and food object

--- snake.py
+++ snake.py
@@ -6,7 +6,7 @@ import random
 WIDTH = 600
 HEIGHT = 600
 SPEED = 50
-SPACE_SIZE = 10
+SPACE_SIZE = 20
 SNAKE_PART = 3
 SNAKE_COLOR = "#00FF00"
 FOOD_COLOR = "#FF0000"
@@ -17,10 +17,23 @@ score = 0
 direction = "down"
 
 class Snake:
-    pass
+    def __init__(self, canvas):
+        self.snake_size = SNAKE_PART
+        self.coordinates = []
+        self.blocks = []
+    
+        for i in range(0, SNAKE_PART):
+            self.coordinates.append([0, 0])
+        for x, y in self.coordinates:
+            block = canvas.create_rectangle(x, y, x + SPACE_SIZE, y  + SPACE_SIZE, fill=SNAKE_COLOR, tags="snake")
+            self.blocks.append(block)
 
 class Food:
-    pass
+    def __init__(self, canvas):
+        x = random.randint(0, (WIDTH / SPACE_SIZE) - 1) * SPACE_SIZE
+        y = random.randint(0, (HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
+        self.coordinates = [x, y]
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tags="food")
 
 def moving():
     pass
@@ -43,5 +56,8 @@ if __name__ == "__main__":
     canvas.pack()
 
     window.update()
+
+    snake = Snake(canvas)
+    food = Food(canvas)
 
     window.mainloop()
