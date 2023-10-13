2fd6f7b5df14bd9a7adc54f1af266bd37945014d
namper <hoangducnam.work@gmail.com>
2023-10-13 23:42:02 +0700

    check collision and set up correct movement

--- snake.py
+++ snake.py
@@ -7,14 +7,15 @@ WIDTH = 600
 HEIGHT = 600
 SPEED = 50
 SPACE_SIZE = 20
-SNAKE_PART = 3
+SNAKE_PART = 30
 SNAKE_COLOR = "#00FF00"
 FOOD_COLOR = "#FF0000"
 BACKGROUND_COLOR = "#000000"
+GAME_OVER_COLOR = "red"
 
 # Initial figure
 score = 0
-direction = "right"
+direction = "down"
 
 class Snake:
     def __init__(self, canvas):
@@ -30,6 +31,7 @@ class Snake:
 
     def snakeMoving(self, canvas, snake, food):
         x, y = self.coordinates[0]
+        #-------Set the snake's movement (add head delete tail)------#
         if direction == "up":
             y -= SPACE_SIZE
         elif direction == "down":
@@ -37,11 +39,45 @@ class Snake:
         elif direction == "right":
             x += SPACE_SIZE
         elif direction == "left":
-            y -= SPACE_SIZE
+            x -= SPACE_SIZE
+
         snake.coordinates.insert(0, (x, y))
         block = canvas.create_rectangle(x, y, x + SPACE_SIZE, y  + SPACE_SIZE, fill=SNAKE_COLOR, tags="snake")
         snake.blocks.insert(0, block)
-        window.after(SPEED, self.snakeMoving, canvas, snake, food)
+
+        del self.coordinates[-1]
+        canvas.delete(self.blocks[-1])
+        del self.blocks[-1]
+        #-------Set the snake's movement (add head delete tail)------#
+
+        # Eat food and generate new ones
+        if(x == food.getCoordinatesX() and y == food.getCoordinatesY()):
+            global score
+            score += 1
+            label.config(text="Score: {}".format(score))
+            canvas.delete("food")
+            food = Food(canvas)
+
+        # Check collisions (itself and the screen sides)
+        if(self.snakeCollisions(snake, canvas)):
+            gameOver(canvas)
+            
+        # Keep moving
+        else:
+            window.after(SPEED, self.snakeMoving, canvas, snake, food)
+
+    def snakeCollisions(self, snake, canvas):
+        x, y = self.coordinates[0]
+
+        if(x < 0 or x >= WIDTH):
+            return True
+        elif(y < 0 or y >= HEIGHT):
+            return True
+        for body_part in self.coordinates[1:]:
+            if(x == body_part[0] and y == body_part[1]):
+                return True
+        return False
+
 
 class Food:
     def __init__(self, canvas):
@@ -49,12 +85,35 @@ class Food:
         y = random.randint(0, (HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
         self.coordinates = [x, y]
         canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tags="food")
+    def getCoordinatesX(self):
+        return self.coordinates[0]
+    def getCoordinatesY(self):
+        return self.coordinates[1]
 
-def changeDirection():
-    pass
 
-def gameOver():
-    pass
+def changeDirection(new_direction):
+    global direction
+
+    if(new_direction == "left"):
+        if(direction != "right"):
+            direction = new_direction
+
+    elif(new_direction == "right"):
+        if(direction != "left"):
+            direction = new_direction
+
+    elif(new_direction == "up"):
+        if(direction != "down"):
+            direction = new_direction
+
+    elif(new_direction == "down"):
+        if(direction != "up"):
+            direction = new_direction 
+
+
+def gameOver(canvas):
+    canvas.delete(ALL)
+    canvas.create_text(canvas.winfo_width()/2, canvas.winfo_height()/2, font=("Ariel", 60), text="GAME OVER" , fill=GAME_OVER_COLOR, tags="gameover")
 
 # Main function
 if __name__ == "__main__":
@@ -72,5 +131,10 @@ if __name__ == "__main__":
     snake = Snake(canvas)
     food = Food(canvas)
     snake.snakeMoving(canvas, snake, food)
-    
+
+    window.bind("<Left>", lambda event: changeDirection("left"))
+    window.bind("<Right>", lambda event: changeDirection("right"))
+    window.bind("<Up>", lambda event: changeDirection("up"))
+    window.bind("<Down>", lambda event: changeDirection("down"))
+
     window.mainloop()
